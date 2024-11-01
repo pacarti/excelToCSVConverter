603fdc8f64952af5a6642487cdb876319276759b
pacarti <a.paciorkowski@protonmail.com>
2024-11-01 20:37:02 +0100

    Step2: Properly named CSV file is created

--- excelToCSVConverter.py
+++ excelToCSVConverter.py
@@ -1,18 +1,27 @@
 import json, os, openpyxl
+from pathlib import Path
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
-for excelFile in os.listdir('.'):
-    # Skip non-xlsx files, load the workbook object.
-    for file in os.listdir(os.getcwd()):
-        if file.endswith('.xlsx'):
-            print(file)
+# Skip non-xlsx files, load the workbook object.
+for file in os.listdir(os.getcwd()):
+    if file.endswith('.xlsx'):
+        # print(file)
+        wb = openpyxl.load_workbook(file)
+        # print(wb.sheetnames)
+    else:
+        continue
+
+    # Loop through every sheet in the workbook.    
+    for sheetName in wb.sheetnames:
+        
+        # Create the CSV filename from the Excel filename and sheet title.
+        pfile = Path(file)
+        # print(pfile.stem + '_' + sheetName + '.csv')
+
+        csvFile = open(pfile.stem + '_' + sheetName + '.csv', 'w')
 '''
-    for sheetName in wb.get_sheet_names():
-        # Loop through every sheet in the workbook.
-        sheet = wb.get_sheet_by_name(sheetName)
-        # TODO: Create the CSV filename from the Excel filename and sheet title.
         # TODO: Create the csv.writer object for this CSV file.
         # Loop through every row in the sheet.
         for rowNum in range(1, sheet.max_row + 1):
