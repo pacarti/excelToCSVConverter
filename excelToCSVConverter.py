a5481b6f2e82fd476d6aa8920599c32ec49b671f
pacarti <a.paciorkowski@protonmail.com>
2024-11-02 22:47:22 +0100

    Step 3: the row content is appended to the rowData list

--- excelToCSVConverter.py
+++ excelToCSVConverter.py
@@ -1,5 +1,6 @@
-import json, os, openpyxl
+import json, os, openpyxl, csv
 from pathlib import Path
+from openpyxl.utils import get_column_letter
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -16,18 +17,24 @@ for file in os.listdir(os.getcwd()):
     # Loop through every sheet in the workbook.    
     for sheetName in wb.sheetnames:
         
+        sheet = wb[sheetName]
+
         # Create the CSV filename from the Excel filename and sheet title.
         pfile = Path(file)
         # print(pfile.stem + '_' + sheetName + '.csv')
 
         csvFile = open(pfile.stem + '_' + sheetName + '.csv', 'w')
-'''
-        # TODO: Create the csv.writer object for this CSV file.
+
+        # Create the csv.writer object for this CSV file.
+        outputWriter = csv.writer(csvFile)
+
         # Loop through every row in the sheet.
         for rowNum in range(1, sheet.max_row + 1):
-            rowData = [] # append each cell to this list
+            rowData = [] # append each cell to this list      
             # Loop through each cell in the row.
             for colNum in range(1, sheet.max_column + 1):
                 # Append each cell's data to rowData.
+                rowData.append(sheet[get_column_letter(colNum) + str(rowNum)].value)
             # Write the rowData list to the CSV file.
-csvFile.close()'''
+            print(rowData)
+csvFile.close()
